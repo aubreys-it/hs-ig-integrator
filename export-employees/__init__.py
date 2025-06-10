68bc66dc851149fcd957d6c3e47dcf43a0b4caac
Aubrey's IT <95295945+aubreys-it@users.noreply.github.com>
2025-06-10 16:32:03 +0000

    Debug

--- export-employees/__init__.py
+++ export-employees/__init__.py
@@ -14,7 +14,7 @@ def create_employee_file(json_data):
     out_file=[]
     i=0
     for row in data:
-        logging.info(f"{i}: {row['EmpId']}")
+        logging.info(f"{i}: {row['EmpID']}")
         CompanyNumber = company_id
         ConceptNumber = concepts[row['StoreNum']]
         StoreNum = row['StoreNum']
