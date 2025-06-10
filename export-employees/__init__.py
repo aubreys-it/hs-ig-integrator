68d412c57a4af228a6273c3e9cd4ed17ae0dcb43
Aubrey's IT <95295945+aubreys-it@users.noreply.github.com>
2025-06-10 16:05:29 +0000

    Change Request Quotes

--- export-employees/__init__.py
+++ export-employees/__init__.py
@@ -59,7 +59,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
     try:
-        req_body = str(req.get_json())
+        req_body = str(req.get_json()).replace("'", '"')
         logging.info(f"Request body: {req_body}")
     except ValueError:
         pass
