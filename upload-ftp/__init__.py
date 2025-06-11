de6c09fbdd24c52fb08b1643748c1c75611c3f3b
Aubrey's IT <95295945+aubreys-it@users.noreply.github.com>
2025-06-11 14:33:08 +0000

    Switch to pysftp

--- upload-ftp/__init__.py
+++ upload-ftp/__init__.py
@@ -1,19 +1,20 @@
 import azure.functions as func
 import logging, os
-from ftplib import FTP
+import pysftp
 from datetime import datetime
 from azure.storage.blob import ContainerClient
 import json
 
-# This Azure Function zips files from a specified Azure Blob Storage container
-# that match today's date and uploads the zip file back to a '__zipped' folder in the same container.
+# This function uploads a zip file to the HS FTP server from Azure Blob Storage
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
 
     ftp_host = os.environ.get('FTP_HOST')
     ftp_user = os.environ.get('FTP_USER')
     ftp_pass = os.environ.get('FTP_PASS')
-    
+    cnopts = pysftp.CnOpts()
+    cnopts.hostkeys = None
+
     container_client = ContainerClient.from_container_url(os.environ.get('UPLOADS_URL'))
     blobs=container_client.list_blobs()
     
@@ -23,10 +24,12 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
             blob_data = blob_client.download_blob().readall()
 
             # Connect to the FTP server
-            with FTP(ftp_host) as ftp:
-                ftp.login(user=ftp_user, passwd=ftp_pass)
+            with sftp.Connection(host=ftp_host, username=ftp_user, password=ftp_pass, cnopts=cnopts) as sftp:
                 # Upload the blob data to the FTP server
-                ftp.storbinary(f'STOR datastore/Import{blob.name}', blob_data)
+                sftp.putfo(
+                    file=blob_data,
+                    remotepath=f'/datastore/Import/{blob.name}'
+                )
 
     req_body = {
         'message': 'Zip file uploaded successfully'
