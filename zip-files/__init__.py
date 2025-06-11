e67752d617b5b00db3ffea76e36905887b6555b2
Aubrey's IT <95295945+aubreys-it@users.noreply.github.com>
2025-06-11 11:26:05 +0000

    Clean Up Code

--- zip-files/__init__.py
+++ zip-files/__init__.py
@@ -2,16 +2,18 @@ import azure.functions as func
 import logging, os, io
 import zipfile
 from datetime import datetime
-from azure.storage.blob import ContainerClient, BlobServiceClient, BlobClient
-from ..modules import core
+from azure.storage.blob import ContainerClient
 import json
+
+# This Azure Function zips files from a specified Azure Blob Storage container
+# that match today's date and uploads the zip file back to a 'zipped' folder in the same container.
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     
     container_client = ContainerClient.from_container_url(os.environ.get('UPLOADS_URL'))
     blobs=container_client.list_blobs()
     zip_buffer = io.BytesIO()
-    zip_file = 'aubreysinc_' + datetime.today().strftime('%Y%m%d_%H%M%S') + '.zip'
+    zip_file_name = 'aubreysinc_' + datetime.today().strftime('%Y%m%d_%H%M%S') + '.zip'
     
     with zipfile.ZipFile(zip_buffer, 'w') as zip_file:
         for blob in blobs:
@@ -22,7 +24,8 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
 
     zip_buffer.seek(0)
 
-    blob_client = container_client.get_blob_client('test.zip')
+    blob_client = container_client.get_blob_client('zipped/' + zip_file_name)
+    # Upload the zip file to the blob storage
     blob_client.upload_blob(zip_buffer.getvalue(), overwrite=True)
     # If req_body is not None, return it as a JSON response
     req_body = {
@@ -37,5 +40,5 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     else:
         return func.HttpResponse(
              "No data provided",
-             status_code=200
+             status_code=400
         )
